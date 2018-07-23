bd50d3e480296c8b814daa766b0665c8e675446a
sunthedeep <sandeep1988@gmail.com>
2018-07-23 12:41:14 -0700

    Updated dada2cluster script to match barcodecounter

--- clusterWithDada2.R
+++ clusterWithDada2.R
@@ -4,35 +4,33 @@ library(Biostrings)
 packageVersion("dada2")
 
 args = commandArgs(trailingOnly=TRUE)
-if (length(args)<2) {
-  stop("Two arguments required: (input file).fastq and outputPrefix", call.=FALSE)
+if (length(args)<3) {
+  stop("Three arguments required: (input file).fastq, (errorSampleFile).fastq and outputPrefix", call.=FALSE)
 } 
 
 #predefined values
 minReadFilter = 10
 
-# get input file and output file prefix
+# get input file, file for making error model and output file prefix
 fastqFileName = args[1]
-outputPrefix = args[2]
+fastqErrorSampleFileName = args[2]
+outputPrefix = args[3]
 filterFastqFilePath = file.path(paste0(outputPrefix,"BCs_filterAndTrimmed.fastq.gz"))
+filterFastqErrorsFilePath = file.path(paste0(outputPrefix,"BCs_filterAndTrimmedForErrors.fastq.gz"))
 
 #fitler and trim input!
 
-filteredAndTrimmed = filterAndTrim(file.path(fastqFileName),filterFastqFilePath,maxN=0,maxEE=c(2,2),truncQ=2,rm.phix=TRUE,compress=TRUE,multithread=FALSE)
+filteredAndTrimmed = filterAndTrim(file.path(fastqFileName),filterFastqFilePath,maxN=0,maxEE=c(2,2),truncQ=2,rm.phix=TRUE,compress=TRUE,multithread=TRUE)
+filteredAndTrimmedErrors = filterAndTrim(file.path(fastqErrorSampleFileName),filterFastqErrorsFilePath,maxN=0,maxEE=c(2,2),truncQ=2,rm.phix=TRUE,compress=TRUE,multithread=TRUE)
 
 #learn errors, dereplicate, run dada and make output table
-errF <- learnErrors(filterFastqFilePath, multithread=FALSE)
+errF <- learnErrors(filteredAndTrimmedErrors, multithread=TRUE)
 derepFs <- derepFastq(filterFastqFilePath, verbose=TRUE)
-dadaFs <- dada(derepFs, err=errF, multithread=FALSE)
+dadaFs <- dada(derepFs, err=errF, multithread=TRUE)
 seqTab <- makeSequenceTable(dadaFs)
 
-#dada2 has a merged paired reads step that does counting and denoising. we are going to skip this since we effectively have single end data
-
-#not going to remove chimeras as this is likely true chimeras anyways.
-
-#where are all of the reads going?
+#identify clustered barcodes and print to fasta output file
 clusteredBCs = colnames(seqTab)
 BCFasta = data.frame(x=paste0(">",seq(1:NROW(clusteredBCs))),seq=clusteredBCs)
 		
 write.table(file=paste0(outputPrefix,"clusteredBCs.fasta"),BCFasta,sep="\n",quote=F,col.names=F,row.names=F)
-#write.table(file=paste0(outputPrefix,"readTrack.tab"),track,sep="\t",quote=F)
